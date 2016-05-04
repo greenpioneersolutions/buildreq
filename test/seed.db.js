dad28c673aa1eb6c8af089fa76e3102188a0c0b4
Jason Humphrey <Humphrey.jason32@gmail.com>
2016-05-04 01:51:37 -0500

    update the seed so that travis recognizes it

--- test/seed.db.js
+++ test/seed.db.js
@@ -1,30 +1,13 @@
 module.exports = seed
 var mongoose = require('mongoose')
-var async = require('async')
+mongoose.Promise = require('bluebird')
 var User = mongoose.model('Users')
 var Blog = mongoose.model('Blog')
 
 function seed (cb) {
-  async.waterfall([
-    function (callback) {
-      async.parallel({
-        User: function (callback) {
-          User.find({}).remove().exec(function () {
-            callback(null, true)
-          })
-        },
-        Blog: function (callback) {
-          Blog.find({}).remove().exec(function () {
-            callback(null, true)
-          })
-        }
-      },
-        function (err, results) {
-          if (err) throw err
-          callback(null, true)
-        })
-    },
-    function (done, callback) {
+  console.log('seed')
+  User.find({}).remove().then(function () {
+    Blog.find({}).remove().then(function () {
       User.create([{
         email: 'jason@greenpioneersolutions.com',
         name: 'jason',
@@ -50,63 +33,54 @@ function seed (cb) {
         name: 'help',
         username: 'help greenpioneer'
       }]).then(function (users) {
-        callback(null, users)
+        Blog.create([{
+          title: 'Mean',
+          user: users[0]._id
+
+        }, {
+          title: 'Green Pioneer Solutions',
+          user: users[0]._id
+
+        }, {
+          title: 'expertise',
+          user: users[1]._id
+
+        }, {
+          title: 'combination',
+          user: users[2]._id
+
+        }, {
+          title: 'goals',
+          user: users[2]._id
+
+        }, {
+          title: 'energy-efficient',
+          user: users[3]._id
+
+        }, {
+          title: 'vision',
+          user: users[4]._id
+
+        }, {
+          title: 'sustainability',
+          user: users[4]._id
+
+        }, {
+          title: 'Sustainable',
+          user: users[5]._id
+
+        }, {
+          title: 'computing',
+          user: users[5]._id
+
+        }, {
+          title: 'example',
+          user: users[5]._id
+
+        }]).then(function (blogs) {
+          if (cb)cb()
+        })
       })
-    },
-    function (users, callback) {
-      Blog.create([{
-        title: 'Mean',
-        user: users[0]._id
-
-      }, {
-        title: 'Green Pioneer Solutions',
-        user: users[0]._id
-
-      }, {
-        title: 'expertise',
-        user: users[1]._id
-
-      }, {
-        title: 'combination',
-        user: users[2]._id
-
-      }, {
-        title: 'goals',
-        user: users[2]._id
-
-      }, {
-        title: 'energy-efficient',
-        user: users[3]._id
-
-      }, {
-        title: 'vision',
-        user: users[4]._id
-
-      }, {
-        title: 'sustainability',
-        user: users[4]._id
-
-      }, {
-        title: 'Sustainable',
-        user: users[5]._id
-
-      }, {
-        title: 'computing',
-        user: users[5]._id
-
-      }, {
-        title: 'example',
-        user: users[5]._id
-
-      }]).then(function (blogs) {
-        callback(null, users, blogs)
-      })
-    }
-  ], function (err, users, blogs) {
-    if (err) throw err
-    cb({
-      users: users,
-      blogs: blogs
     })
   })
 }
